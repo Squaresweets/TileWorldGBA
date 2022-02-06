43f2514748be8541b34ce8eb9c2146ff005d1377
Matthew Collier <matthew@matthewcollier.co.uk>
2022-02-06 21:11:23 +0000

    Camera now tracks player

--- source/main.c
+++ source/main.c
@@ -13,6 +13,9 @@
 
 #define SCREEN_W      240
 #define SCREEN_H      160
+//Screen offset from player
+#define SCREEN_O_W    116.0
+#define SCREEN_O_H    76.0
 
 #define CROSS_TX 15
 #define CROSS_TY 10
@@ -31,6 +34,11 @@ u32 se_index(u32 tx, u32 ty, u32 pitch)
 	return sbb*1024 + ((tx&31)+(ty&31)*32);
 }
 
+float lerp(float a, float b, float f)
+{
+    return a + f * (b - a);
+}
+
 void init_map()
 {
 	int ii, jj, c;
@@ -39,6 +47,8 @@ void init_map()
 	REG_BG0CNT= BG_CBB(CBB_0) | BG_SBB(SBB_0) | BG_REG_64x64;
 	REG_BG0HOFS= 0;
 	REG_BG0VOFS= 0;
+	bg0_pt.x = 0;
+	bg0_pt.y = 0;
 
 	// create the tiles: basic tile and a cross
     memcpy(&tile_mem[CBB_0][0], TileMapTiles, TileMapTilesLen);
@@ -78,7 +88,7 @@ int main()
 	REG_DISPCNT= DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ;
     oam_init(obj_buffer, 128);
 
-	int x = (SCREEN_W/2)-4, y= (SCREEN_H/2)-4;
+	float playerx = 0, playery = 0;
     u32 tid= 12, pb= 0;      // (3) tile id, pal-bank
     OBJ_ATTR *player= &obj_buffer[0];
 
@@ -86,22 +96,26 @@ int main()
         ATTR0_SQUARE,              // Square, regular sprite
         ATTR1_SIZE_8,              // 8x8p, 
         ATTR2_PALBANK(pb) | tid);   // palbank 0, tile 0
-	
-    // (4) position sprite (redundant here; the _real_ position
-    // is set further down
-    obj_set_pos(player, x, y);
-
 	while(1)
 	{
 		vid_vsync();
 
 		key_poll();
-		bg0_pt.x += key_tri_horz();
-		bg0_pt.y += key_tri_vert();
+		//bg0_pt.x += key_tri_horz();
+		//bg0_pt.y += key_tri_vert();
+		playerx += key_tri_horz();
+		playery += key_tri_vert();
+		
+		//bg0_pt.x = (lerp(playerx, bg0_pt.x + SCREEN_O_W, 0.01f)) - SCREEN_O_W;
+		//bg0_pt.y = (lerp(playery, bg0_pt.y + SCREEN_O_H, 0.01f)) - SCREEN_O_H;
+		bg0_pt.x = playerx - SCREEN_O_W;
+		bg0_pt.y = playery - SCREEN_O_H;
+
+		//Place player at correct position on the screen
+    	obj_set_pos(player, playerx - bg0_pt.x, playery - bg0_pt.y);
+        oam_copy(oam_mem, obj_buffer, 1);   // (6) Update OAM (only one now)
 
 		REG_BG_OFS[0]= bg0_pt;	// write new position
-
-        oam_copy(oam_mem, obj_buffer, 1);   // (6) Update OAM (only one now)
 	}
 	return 0;
 }
